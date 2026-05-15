cdb0207d26696bd1575601b062f84192e9d98ae6
xanni33 <ania_gruca@interia.pl>
2026-05-15 23:19:24 +0200

    add treshold tuning

---
+++ src/losses.py
@@ -0,0 +1,24 @@
+﻿import torch
+import torch.nn as nn
+
+def get_pos_weights(y_train):
+
+    y = torch.tensor(y_train)
+
+    pos_counts = y.sum(dim=0)
+    neg_counts = len(y) - pos_counts
+
+    weights = neg_counts / (pos_counts + 1e-6)
+
+    return weights
+
+
+def create_weighted_loss(y_train):
+
+    pos_weights = get_pos_weights(y_train)
+
+    criterion = nn.BCEWithLogitsLoss(
+        pos_weight=pos_weights
+    )
+
+    return criterion
--- src/model.py
+++ src/model.py
@@ -1,24 +1,82 @@
 #1d cnn
 import torch.nn as nn
 
+
 class ECGCNN(nn.Module):
+
     def __init__(self, num_classes=5):
+
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv1d(1, 16, 7, padding=3),
+
+        #feature extraction
+        self.features = nn.Sequential(
+
+            nn.Conv1d(
+                1,
+                32,
+                kernel_size=7,
+                padding=3
+            ),
+
+            nn.BatchNorm1d(32),
+
             nn.ReLU(),
+
             nn.MaxPool1d(2),
 
-            nn.Conv1d(16, 32, 5, padding=2),
+            nn.Conv1d(
+                32,
+                64,
+                kernel_size=5,
+                padding=2
+            ),
+
+            nn.BatchNorm1d(64),
+
             nn.ReLU(),
+
             nn.MaxPool1d(2),
+
+            nn.Conv1d(
+                64,
+                128,
+                kernel_size=3,
+                padding=1
+            ),
+
+            nn.BatchNorm1d(128),
+
+            nn.ReLU(),
+
+            nn.MaxPool1d(2),
+
+            nn.Dropout(0.3)
+        )
+
+        #classifier
+        self.classifier = nn.Sequential(
 
             nn.Flatten(),
-            nn.Linear(32 * 180, 128),
+
+            nn.Linear(
+                128 * 90,
+                256
+            ),
+
             nn.ReLU(),
-            nn.Linear(128, num_classes),
-            #nn.Sigmoid() poprawka bo potem i tak bedzie w lossie:)
+
+            nn.Dropout(0.5),
+
+            nn.Linear(
+                256,
+                num_classes
+            )
         )
 
     def forward(self, x):
-        return self.net(x)
+
+        x = self.features(x)
+
+        x = self.classifier(x)
+
+        return x
